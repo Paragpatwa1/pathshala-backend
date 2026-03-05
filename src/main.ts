4839fffb0e6147e24e9f4201c782070f443b644f
Parag Patwa <paragpatwa10@gmail.com>
2026-03-06 01:31:32 +0530

    fix railway start path

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,9 @@ async function bootstrap() {
 
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
+const port = process.env.PORT || 3000;
+await app.listen(port, '0.0.0.0');
 
- await app.listen(process.env.PORT || 5000);
+console.log(`🚀 Server running on port ${port}`);
 }
 bootstrap();
