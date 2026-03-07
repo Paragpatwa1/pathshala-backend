9957c306cb7a4e0f855025a162c014ebb9a85bb6
Parag Patwa <paragpatwa10@gmail.com>
2026-03-07 16:06:44 +0530

    fix railway start path

--- src/modules/payment/dto/create-order.dto.ts
+++ src/modules/payment/dto/create-order.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
 
 export class CreateOrderDto {
 
   @ApiProperty({
-    example: 960,
+    example: 800,
     description: 'Amount in INR',
   })
+  @IsNumber()
   amount: number;
-
 }
